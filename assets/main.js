4f2a46f9451773cf8ea7ced0419df88d3d0fdd2d
Ezequiel <eze3769@gmail.com>
2021-07-17 17:13:35 -0300

    Add english traslation functionality

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,40 @@
-const langChange = ()=>{
-    console.log("listo")
-    fetch('./assets/eng.json')
-    .then(res => res.json())
-    .then(res =>{
-        console.log(res)
-        console.log(res.nav1)
-
-    })
-
+const spaFlag = "images/spa-lang.png"
+const engFlag = "images/eng-lang.png"
+const langStorage = localStorage.getItem("lang")
+if(langStorage != null){
+    selected = langStorage
+    console.log(selected)
+}else{
+    selected = "spa"
 }
 
-addEventListener('click',langChange)
+const langChange = (option)=>{
+    let setTo;
+    let setFrom
+    if (option === "eng"){
+        setTo = "eng"
+        setFrom = "spa"
+    }else{
+        if(selected === "spa"){
+            setTo = "eng"
+            setFrom = "spa"
+        }else{
+            setTo ="spa"
+            setFrom = "eng"
+        }
+    }
+    
+    
+
+    $(`.${setTo}-text`).css("display","unset")
+    $(`.${setFrom}-text`).css("display","none")
+    document.getElementById("lang-button").src = `images/${setFrom}-lang.png`
+    
+    localStorage.setItem("lang", setTo)
+    selected = setTo
+}
+if(selected === "eng"){
+    langChange("eng")
+}
+document.getElementById("lang-button").addEventListener('click',langChange)
 
