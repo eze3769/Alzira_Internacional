0b8cd000d9fff1bf7c0c89b0e17124b513d334b6
Ezequiel <eze3769@gmail.com>
2021-07-22 16:01:03 -0300

    fix mailer

--- assets/form.js
+++ assets/form.js
@@ -1,19 +1,55 @@
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
+emailjs.init("user_bldGjbxI6Q2mwlO0yl8Vd"); 
+
+const contactAction = ()=>{
+    const name = document.getElementById("validationCustom01")
+    const email = document.getElementById("validationCustom02")
+    const message = document.getElementById("validationCustom03")
+    const subject = document.getElementById("validationCustom04")
+
+    let data = {
+        from_name: name,
+        from_email: email,
+        message: message,
+        subject: subject
+    }
+    emailjs.send("service_fxhk4pv","template_9u99mee", data)
+    .then(function(response) {
+       console.log(response)
+        if(response.text === 'OK'){
+            if (selected == "eng"){
+                
+            }
+            Swal.fire({
+              title: selText()[0],
+              icon: 'success',
+              html: selText()[1],
+              focusConfirm: true,
+              confirmButtonText:'Ok',
+              confirmButtonAriaLabel: 'Thumbs up, great!',
+              cancelButtonText:
+              '<i class="fa fa-thumbs-down"></i>',
+                cancelButtonAriaLabel: 'Thumbs down'
+            })
+            }
+    console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+    }, function(err) {
+        alert('Ocurrió un problema al enviar el correo');
+    console.log("FAILED. error=", err);
+    });
+}
+
+window.addEventListener('load', function() {
+    const form = document.getElementById('contact-form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
         if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
+            event.stopPropagation()
         }
         form.classList.add('was-validated');
+        if(form.checkValidity() === true){
+            contactAction()
+        }   
         }, false);
-    });
     }, false);
-})();
 
 
